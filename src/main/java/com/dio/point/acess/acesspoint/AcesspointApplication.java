6bec0c174d1a6fa7c712dbd952053214bc3cd36c
Ryam Sousa <ryam.sousa123@gmail.com>
2021-07-08 17:25:10 -0300

    initial commit

---
+++ src/main/java/com/dio/point/acess/acesspoint/AcesspointApplication.java
@@ -0,0 +1,13 @@
+package com.dio.point.acess.acesspoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcesspointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcesspointApplication.class, args);
+	}
+
+}
